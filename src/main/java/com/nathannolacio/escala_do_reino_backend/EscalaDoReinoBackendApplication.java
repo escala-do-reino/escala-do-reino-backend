f334c05a21b38ecd5959080457cae5d1540f3ad9
nathannolacio <nathannolacio04@gmail.com>
2025-12-11 18:11:30 -0300

    :sparkles: feat: primeiro commit - estrutura do projeto

---
+++ src/main/java/com/nathannolacio/escala_do_reino_backend/EscalaDoReinoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nathannolacio.escala_do_reino_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalaDoReinoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalaDoReinoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathannolacio/escala_do_reino_backend/EscalaDoReinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathannolacio.escala_do_reino_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalaDoReinoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
